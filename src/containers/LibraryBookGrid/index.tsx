7fdf3792bffc23fd81ed828d178a0ef8edaa0bf5
Richard Cunha <decunha@me.com>
2021-02-17 15:41:50 -0500

    wip

--- src/containers/LibraryBookGrid/index.tsx
+++ src/containers/LibraryBookGrid/index.tsx
@@ -1,18 +1,43 @@
-import React, {FunctionComponent} from "react";
-import { useDispatch } from "react-redux";
+import React, {FunctionComponent, ReactElement} from "react";
+import {useDispatch} from "react-redux";
 import shortid from "shortid";
+import chunk from "lodash/chunk";
 
-import { deleteBook } from "../../api/library";
+import {deleteBook} from "../../api/library";
 import Book from "../../components/Book";
-import { removeBook } from "../../state/slices/library";
+import {removeBook} from "../../state/slices/library";
 import {LibraryBookGridContainer, GridItem} from "./styles";
 
 type LibraryBookGridProps = {
   books: string[];
 };
 
+const MAX_GRID_COLUMNS = 6;
+
 const LibraryBookGrid: FunctionComponent<LibraryBookGridProps> = ({books}) => {
   const dispatch = useDispatch();
+
+  const buildLibraryShelves = (): ReactElement[] => {
+    const chunks = chunk(books, MAX_GRID_COLUMNS);
+    const items = [];
+
+    for (let chunk = 0; chunk < chunks.length; chunk++) {
+      for (let book = 0; book < chunks[chunk].length; book++) {
+        items.push(
+          <GridItem key={shortid.generate()} column={book + 1} row={chunk + 1}>
+            <Book title={chunks[chunk][book]} handleOnDelete={handleBookDelete} />
+          </GridItem>
+        );
+      }
+    }
+
+    return items;
+  };
+
+  const getMaxGridRows = (): number => {
+    const numberOfBooks = books.length;
+    return Math.floor(numberOfBooks / MAX_GRID_COLUMNS);
+  };
 
   const handleBookDelete = async (title: string) => {
     try {
@@ -24,12 +49,8 @@ const LibraryBookGrid: FunctionComponent<LibraryBookGridProps> = ({books}) => {
   };
 
   return (
-    <LibraryBookGridContainer>
-      {books.map((book, index) => (
-        <GridItem key={shortid.generate()} column={index + 1}>
-          <Book title={book} handleOnDelete={handleBookDelete} />
-        </GridItem>
-      ))}
+    <LibraryBookGridContainer rows={getMaxGridRows()} columns={MAX_GRID_COLUMNS}>
+      {buildLibraryShelves()}
     </LibraryBookGridContainer>
   );
 };
--- src/containers/LibraryBookGrid/styles.tsx
+++ src/containers/LibraryBookGrid/styles.tsx
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 
+type LibraryBookGridContainerProps = {
+  columns: number;
+  rows: number;
+};
+
 type GridItemProps = {
   column: number;
   row?: number;
 };
 
-export const LibraryBookGridContainer = styled.div`
+export const LibraryBookGridContainer = styled.div<LibraryBookGridContainerProps>`
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: ${({columns}) => `repeat(${columns}, 1fr)`};
+  grid-template-rows: ${({rows}) => `repeat(${rows}, 1fr)`};
   column-gap: 10px;
   row-gap: 10px;
 `;
 
 export const GridItem = styled.div<GridItemProps>`
   grid-column: ${({column}) => column};
-  grid-row: 1;
+  grid-row: ${({row}) => row};
 `;
