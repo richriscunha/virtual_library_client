504ce4399550986745195c0a32bdd8834c72bb8f
Richard Cunha <decunha@me.com>
2021-02-17 17:08:43 -0500

    wip

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_PORT = process.env.REACT_APP_API_PORT || 3030;
+
 const instance = axios.create({
-  baseURL: "http://localhost:3030/api/books",
+  baseURL: `http://localhost:${API_PORT}/api/books`,
   timeout: 3500,
 });
 
